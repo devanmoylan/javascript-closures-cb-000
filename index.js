7bbd6d7ab27feed5101448899e780830665386c5
Devan <devan.moylan@backyardlion.com>
2017-08-26 13:03:48 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-const app = "I don't do much."
+const app = "I don't do much.";
+
+function fatBastard(meal) {
+  return whatsForDinner() {
+    if (meal === 'Mini-Me') { // whatsForDinner() uses argument provided to the parent function
+      console.log('The wee man is in my belly!');
+    } else {
+      console.log(`I'm eatin' a bit of ${meal}! Burp.`);
+    }
+  }
+
+  return whatsForDinner;
+}
+
+const whatsForDinner = fatBastard('Kobe beef');
+
+const whatsInHisTummy = fatBastard('Mini-Me');
+
+whatsInHisTummy();
+
+whatsForDinner();
